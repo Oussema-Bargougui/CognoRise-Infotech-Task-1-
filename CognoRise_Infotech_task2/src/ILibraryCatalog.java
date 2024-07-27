1eaf15877a3e9df702b91c9c21bdbe4f5ac1b6aa
Oussema Bargougui <bargouguioussama80@gmail.com>
2024-07-27 20:53:03 +0100

    Delete CognoRise_Infotech_task2 directory

--- CognoRise_Infotech_task2/src/ILibraryCatalog.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public interface ILibraryCatalog {
-    public  abstract void ajouter(Book book);
-    public abstract Book chercherbyTA (String titre, String author);
-    public abstract List<Book> afficher ();
-    public abstract boolean checkOut(String title, String author) ;
-}
